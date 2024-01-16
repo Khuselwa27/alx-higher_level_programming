560b1021d422ec898c31d98446e3eaa7b340adbb
khuselwa27 <khuselwa.xhoma@icloud.com>
2024-01-15 16:10:58 -0800

    task complete

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
-            print(name)
+    from hidden_4 import *
+    allf = dir()
+    for i in range (0, len(allf)):
+        if allf[i][:2] != "__":
+            print("{:s}".format(allf[i]))
